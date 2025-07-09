485d2deb1bc10f147bc56d51c559c235254892d0
Dmitry <d.a.demkin@gmail.com>
2025-07-09 19:47:51 +0500

    finish

--- main.go
+++ main.go
@@ -1,40 +1,52 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"sync"
+	"time"
 )
 
-func worker(id int, jobs <-chan int, wg *sync.WaitGroup, errChan chan<- error) {
+func worker(ctx context.Context, id int, jobs <-chan int, wg *sync.WaitGroup, errChan chan<- error) {
 	defer wg.Done()
 	for {
-		job, ok := <-jobs
-		if !ok {
+		select {
+		case job, ok := <-jobs:
+			if !ok {
+				return
+			}
+
+			if job%2 != 0 {
+				errChan <- fmt.Errorf("Worker %d: ошибка при обработке %d", id, job)
+				continue
+			}
+
+			fmt.Printf("Worker %d отработал задачу %d\n", id, job)
+			time.Sleep(500 * time.Millisecond)
+		case <-ctx.Done():
+			fmt.Printf("Worker %d завершил работу из-за отмены\n", id)
 			return
 		}
-
-		if job%2 != 0 {
-			errChan <- fmt.Errorf("Worker %d: ошибка при обработке %d", id, job)
-			continue
-		}
-
-		fmt.Printf("Worker %d отработал задачу %d\n", id, job)
 	}
 }
 
 func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
 	jobs := make(chan int)
 	var wg sync.WaitGroup
 	errChan := make(chan error)
 	for i := 1; i <= 3; i++ {
 		wg.Add(1)
-		go worker(i, jobs, &wg, errChan)
+		go worker(ctx, i, jobs, &wg, errChan)
 	}
 	go errorsFinder(errChan)
 	for i := 1; i <= 10; i++ {
 		jobs <- i
 	}
+	time.Sleep(1 * time.Second)
+	cancel()
 	close(jobs)
 
 	wg.Wait()
